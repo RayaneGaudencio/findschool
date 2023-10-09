f1106f9f289503280a12f1a2d2accd92d52a8032
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-10-09 15:20:09 -0300

    Fundo no tom e logo da FindSchool centralizado

---
+++ src/components/FundoComLogo.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const FundoComLogo = styled.div`
+  background-color: #150096;
+  width: 50vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+export default FundoComLogo
