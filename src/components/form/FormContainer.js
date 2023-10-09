7a4fc1a492451afea5b8a304eacb5f21259fc27a
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-10-09 19:24:30 -0300

    componente alterado para form e adição do tamanho máximo do container

--- src/components/form/FormContainer.js
+++ src/components/form/FormContainer.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
-const FormContainer = styled.div`
+const FormContainer = styled.form`
   background-color: white;
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
   margin: auto;
+  max-width: 18rem;
+  flex-wrap: wrap;
 `;
 
 const CenteredContent  = styled.div`
