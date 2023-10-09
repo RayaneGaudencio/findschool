9dff750d7a23d3ecf91b519f96d7d22ca4fc2a03
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-10-09 15:25:17 -0300

    componente util para alinhar itens

---
+++ src/utils/DisplayFlexRow.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+// coloca os itens filhos lado a lado
+const DisplayFlexRow = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: white;
+`;
+
+export default DisplayFlexRow 
