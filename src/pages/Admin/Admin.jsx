c300747c38ac6f9045df0f5cebdf90fe2c3ec7fc
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-10-09 18:00:26 -0300

    adicionado diretorio de pages

---
+++ src/pages/Admin/Admin.jsx
@@ -0,0 +1,11 @@
+import CadastroAdmin from "../../admin/CadastroAdmin";
+
+const Admin = () => {
+    return (
+        <div>
+            <CadastroAdmin />
+        </div>
+    )
+}
+
+export default Admin;
