2915f4e12e582ff68a28ae6ee35ebc0a4a31bb78
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-10-14 17:13:27 -0300

    componente de cadastro atualizado para os padrões de uso dos hooks

--- src/pages/Admin/CadastroAdmin.jsx
+++ src/pages/Admin/CadastroAdmin.jsx
@@ -14,108 +14,144 @@ import DescricaoDadoIncorreto from '../../components/form/DadoIncorreto';
 
 const CadastroAdmin = () => {
   const [formData, setFormData] = useState({
-    nome: '',
-    email: '',
-    cpf: '',
-    senha: ''
+    nome: "",
+    email: "",
+    cpf: "",
+    senha: ""
   });
 
-  const [errors, setErrors] = useState({
-    nome: '',
-    email: '',
-    cpf: '',
-    senha: ''
+  const [erros, setErros] = useState({
+    nome: { temErros: false, mensagem: "" },
+    email: { temErros: false, mensagem: "" },
+    cpf: { temErros: false, mensagem: "" },
+    senha: { temErros: false, mensagem: "" }
   });
 
-  const handleCadastroAdmin = async (e) => {
-    e.preventDefault()
+  const [formErros, setFormErros] = useState({ erros: false, mensagem: ""})
 
-    const newErrors = {};
-
-    const { nome, email, cpf, senha } = formData;
-
-    if (!nome) {
-      alert('Por favor, preencha o campo Nome.');
-      return;
+  const enviarFormulario = async (e) => {
+    for (const campo in formData) {
+      if (formData[campo].trim() === '') {
+        setFormErros({erros: true, mensagem: "Preencha os campos necessários antes de cadastrar."})
+      } else {
+        setFormErros({erros: false, mensagem: ""})
+      }
     }
 
-    if (!email) {
-      alert('Por favor, preencha o campo Email.');
-      return;
-    }
-
-    if (!cpf) {
-      alert('Por favor, preencha o campo CPF.');
-      return;
-    }
-
-    if (!validateNomeCompleto(formData.nome)) {
-      newErrors.nome = 'Deve conter nome e sobrenome.';
-    } 
-
-    if (!senha) {
-      alert('Por favor, preencha o campo Senha.');
-      return;
-    }
-
-    if (!validateEmail(formData.email)) {
-      newErrors.email = 'Email inválido';
-    }
-  
-    if (!validateCPF(formData.cpf)) {
-      newErrors.cpf = 'CPF inválido';
-    }
-  
-    if (!validateSenha(formData.senha)) {
-      newErrors.senha = 'Senha deve ter pelo menos 8 caracteres, contendo pelo menos uma letra maíuscula, um número e um símbolo.';
-    }
-
-    setErrors(newErrors);
-
-    if (Object.keys(newErrors).length === 0) {
-    try {
+    if (!formErros.erros) {
+      try {
         const response = await axios.post(`${API_URL}/admins`, formData, {
           withCredentials: true
         });
         console.log('Administrador cadastrado com sucesso:', response.data);
-    } catch (error) {
+        // window.location.reload();
+      } catch (error) {
         console.error('Erro ao cadastrar administrador:', error);
+      }
     }
-  }
   };
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    setFormData({
+      ...formData,
+      [name]: value
+    });
   };
 
-return (
-  <DisplayFlexRow>
-    <FundoComLogo>
+  const verificaPreenchimentoCampos = (name) => {
+    if (name == "nome") {
+      if (formData.nome.trim() === '') {
+        setErros({ ...erros, nome: { temErros: true, mensagem: 'Campo obrigatório.'}})
+      } else if (!validateNomeCompleto(formData.nome)){
+          setErros({ ...erros, nome: { temErros: true, mensagem: 'Deve conter nome e sobrenome.'}})
+      } else {
+        setErros({...erros, nome:({ temErros: false})})
+      }
+    } else if (name == "email") {
+        if (formData.email.trim() == '') {
+          setErros({ ...erros, email: { temErros: true, mensagem: 'Campo obrigatório.'}})
+        }else if (!validateEmail(formData.email)){
+            setErros({ ...erros, email: { temErros: true, mensagem: 'Email inválido.'}})
+        } else {
+          setErros({...erros, email:({ temErros: false})})
+        }
+    } else  if (name == "cpf") {
+        if (formData.cpf.trim() == '') {
+          setErros({ ...erros, cpf: { temErros: true, mensagem: 'Campo obrigatório.'}})
+        }else if (!validateCPF(formData.cpf)){
+            setErros({ ...erros, cpf: { temErros: true, mensagem: 'CPF inválido.'}})
+        } else {
+          setErros({...erros, cpf:({ temErros: false})})
+        }
+    }else if (name == "senha") {
+        if (formData.senha.trim() == '') {
+          setErros({ ...erros, senha: { temErros: true, mensagem: 'Campo obrigatório.'}})
+        }else if (!validateSenha(formData.senha)){
+            setErros({ ...erros, senha: { temErros: true, mensagem: 'Senha deve ter pelo menos 8 caracteres, contendo pelo menos uma letra maíuscula, um número e um símbolo.'}})
+        } else {
+          setErros({...erros, senha:({ temErros: false})})
+        }
+      } else {
+        setValido(true)
+      }
+  }
+
+
+  return (
+    <DisplayFlexRow>
+      <FundoComLogo>
         <img src={LogoSVG} alt="Logo branca FindSchool" style={{ width: '500px', height: 'auto' }}></img>
-    </FundoComLogo>
-    <FormContainer onSubmit={handleCadastroAdmin}>
-      <CenteredContent>
-        <Title2>Cadastrar Administrador</Title2>
-      </CenteredContent>
-      <DescricaoSobInput>Nome:</DescricaoSobInput>
-      <Input type="text" name='nome' value={formData.nome} onChange={handleInputChange}/>
-      {errors.nome && <DescricaoDadoIncorreto>{errors.nome}</DescricaoDadoIncorreto>}
-      <DescricaoSobInput>Email:</DescricaoSobInput>
-      <Input type="email"  name='email' value={formData.email} onChange={handleInputChange}/>
-      {errors.email && <DescricaoDadoIncorreto>{errors.email}</DescricaoDadoIncorreto>}
-      <DescricaoSobInput>CPF:</DescricaoSobInput>
-      <Input type="text" name="cpf" value={formData.cpf} onChange={handleInputChange}/>
-      {errors.cpf && <DescricaoDadoIncorreto>{errors.cpf}</DescricaoDadoIncorreto>}
-      <DescricaoSobInput>Senha:</DescricaoSobInput>
-      <Input type="password" name="senha" value={formData.senha} onChange={handleInputChange} />
-      {errors.senha && <DescricaoDadoIncorreto>{errors.senha}</DescricaoDadoIncorreto>}
-      <CenteredContent>
-        <Button type="submit" onClick={handleCadastroAdmin}>Cadastrar</Button>
-      </CenteredContent>
-    </FormContainer>
-  </DisplayFlexRow>
-);
+      </FundoComLogo>
+      <FormContainer onSubmit={(event) => {
+        event.preventDefault();
+        enviarFormulario(formData);
+      }}>
+        <CenteredContent>
+          <Title2>Cadastrar Administrador</Title2>
+        </CenteredContent>
+        <DescricaoSobInput>Nome:</DescricaoSobInput>
+        <Input
+          type="text"
+          name="nome"
+          value={formData.nome}
+          onChange={handleInputChange}
+          onBlur={(e) => verificaPreenchimentoCampos(e.target.name)}
+        />
+        {erros.nome.temErros && <DescricaoDadoIncorreto>{erros.nome.mensagem}</DescricaoDadoIncorreto>}
+        <DescricaoSobInput>Email:</DescricaoSobInput>
+        <Input type="email"
+          name='email'
+          value={formData.email}
+          onChange={handleInputChange}
+          onBlur={(e) => verificaPreenchimentoCampos(e.target.name)}
+        />
+        {erros.email.temErros && <DescricaoDadoIncorreto>{erros.email.mensagem}</DescricaoDadoIncorreto>}
+        <DescricaoSobInput>CPF:</DescricaoSobInput>
+        <Input
+          type="text"
+          name="cpf"
+          value={formData.cpf}
+          onChange={handleInputChange}
+          onBlur={(e) => verificaPreenchimentoCampos(e.target.name)}
+        />
+        {erros.cpf.temErros && <DescricaoDadoIncorreto>{erros.cpf.mensagem}</DescricaoDadoIncorreto>}
+        <DescricaoSobInput>Senha:</DescricaoSobInput>
+        <Input
+          type="password"
+          name="senha"
+          value={formData.senha}
+          onChange={handleInputChange}
+          onBlur={(e) => verificaPreenchimentoCampos(e.target.name)}
+        />
+        {erros.senha.temErros && <DescricaoDadoIncorreto>{erros.senha.mensagem}</DescricaoDadoIncorreto>}
+        <CenteredContent>
+           {formErros.erros && <DescricaoDadoIncorreto>{formErros.mensagem}</DescricaoDadoIncorreto>}
+          <Button type="submit">Cadastrar</Button>
+        </CenteredContent>
+      </FormContainer>
+    </DisplayFlexRow>
+  );
 };
 
 export default CadastroAdmin;
