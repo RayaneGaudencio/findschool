59d34baf921918e0b38e43f3c9bb9d90bcb2e5c9
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-10-15 14:36:49 -0300

    rota para tela de login do administrador

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CadastroAdmin from "./pages/Admin/CadastroAdmin";
 import HomeAdmin from "./pages/Admin/HomeAdmin";
+import LoginAdmin from "./pages/Admin/LoginAdmin";
+
 
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/cadastro_admin" Component={CadastroAdmin}/>
         <Route path="/admin" Component={HomeAdmin}/>
+        <Route path="/login_admin" Component={LoginAdmin}/>
       </Routes>
     </Router>
   );
